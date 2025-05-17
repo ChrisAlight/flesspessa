668e42bcde2c876f4312bbf157ca238535282c3d
Chris Light <cajohnson99@hotmail.com>
2025-05-17 17:18:32 -0400

    Dynamic questions (#2)
    * made easy mode dynamic
    
    * Bugfix: preventing multiple answers for one question
    
    * Moved JS to app.js
    
    * Added country flags
    
    * Creating answer buttons dynamically
    
    * Added back button

---
+++ app.js
@@ -0,0 +1,77 @@
+
+class App {
+    countries;
+    nextDifficulty;
+
+    hasAnswered = false;
+    correctAnswer;
+    correctAnswerCount = 0;
+
+    constructor(countries, nextDifficulty) {
+        this.countries = countries;
+        this.nextDifficulty = nextDifficulty;
+        this.generateAnswerButtons();
+        this.setRandomFlag();
+    }
+
+    generateAnswerButtons() {
+        const answerButtonContainer = document.getElementById('answerButtons');
+        answerButtonContainer.innerHTML = '';
+
+        for (const country of this.countries) {
+            const answerButton = document.createElement('button');
+            answerButton.innerHTML = country.name;
+            answerButton.onclick = () => this.answer(country.name);
+            answerButtonContainer.appendChild(answerButton);
+        }
+    }
+
+    getRandomCountry() {
+        const randomIndex = Math.floor(Math.random() * countries.length);
+        return countries.splice(randomIndex, 1)[0];
+    }
+
+    setRandomFlag() {
+        const randomCountry = this.getRandomCountry();
+        const flagImage = document.getElementById('flagImage');
+        flagImage.src = `assets/flags/${randomCountry.code}.webp`;
+        this.correctAnswer = randomCountry;
+    }
+
+    answer(selection) {
+        if (this.hasAnswered) {
+            return;
+        }
+        this.hasAnswered = true;
+
+        const result = document.getElementById('result');
+        const resultMessage = document.getElementById('resultMessage');
+        const resultLink = document.getElementById('resultLink');
+        const wasCorrect = selection === this.correctAnswer.name;
+
+        result.style = 'display: inherit';
+
+        if (wasCorrect) {
+            this.correctAnswerCount++;
+            resultMessage.innerHTML = `Correct! The flag is for ${this.correctAnswer.name}.`;
+            resultLink.innerHTML = this.correctAnswerCount < 3 ? 'Next Question' : this.nextDifficulty ? `try ${this.nextDifficulty}` : 'back to menu';
+            resultLink.onclick = () => {
+                if (this.correctAnswerCount < 3) {
+                    this.setRandomFlag();
+                    result.style.display = 'none';
+                } else {
+                    window.location.href = `${this.nextDifficulty ?? 'index'}.html`;
+                }
+                this.hasAnswered = false;
+            };
+        } else {
+            resultMessage.innerHTML = 'Incorrect!';
+            resultLink.innerHTML = 'Try Again';
+            resultLink.onclick = () => {
+                result.style.display = 'none';
+                this.hasAnswered = false;
+            };
+        }
+
+    }
+}
